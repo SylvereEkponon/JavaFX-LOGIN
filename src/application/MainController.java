2611634d23ebafc67b555a858281ff2159315ef3
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-11-02 12:45:25 -0400

    Edited MainController class

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,5 +1,28 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class MainController {
 
+	@FXML
+	private Label lblStatus;
+	
+	@FXML
+	private TextField txtPassword;
+	
+	@FXML
+	private TextField txtUserName;
+	
+	private void Login(ActionEvent event) {
+		
+		if (txtUserName.getText().equals("username") && txtPassword.getText().equals("password")) {
+			lblStatus.setText("Login Success");
+		}else {
+			lblStatus.setText("Login Failed");
+		}
+
+	}
 }
